f7c2fc709bd4ffc44b6a95a9d73b933ceaec56ce
mooziii <63669478+mooziii@users.noreply.github.com>
2022-05-19 13:46:55 +0200

    Initial port to 1.19

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
-    kotlin("jvm") version "1.6.20"
-    kotlin("plugin.serialization") version "1.6.20"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.serialization") version "1.6.21"
     id("fabric-loom") version "0.12-SNAPSHOT"
     java
 }
 
 group = "me.obsilabor"
-version = "1.2-SNAPSHOT"
+version = "1.3-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -15,14 +15,18 @@ repositories {
 }
 
 dependencies {
+    // kotlin
     implementation(kotlin("stdlib"))
-    implementation("me.obsilabor:alert:1.0.4")
-    include("me.obsilabor:alert:1.0.4")
-    minecraft("com.mojang:minecraft:1.18.2")
-    mappings("net.fabricmc:yarn:1.18.2+build.2")
-    modImplementation("net.fabricmc:fabric-loader:0.13.3")
-    modImplementation("net.fabricmc:fabric-language-kotlin:1.7.2+kotlin.1.6.20")
     api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+    // event system
+    implementation("me.obsilabor:alert:1.0.5")
+    include("me.obsilabor:alert:1.0.5")
+    // fabric
+    minecraft("com.mojang:minecraft:1.19-pre1")
+    mappings("net.fabricmc:yarn:1.19-pre1+build.1")
+    modImplementation("net.fabricmc:fabric-loader:0.14.5")
+    modImplementation("net.fabricmc:fabric-language-kotlin:1.7.4+kotlin.1.6.21")
+    // modmenu & clothconfig
     modApi("com.terraformersmc:modmenu:3.1.0")
     modApi("me.shedaniel.cloth:cloth-config-fabric:6.2.57") {
         exclude("net.fabricmc.fabric-api")
